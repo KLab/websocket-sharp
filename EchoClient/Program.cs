5ddcfb93838b5f5673a32e9f7a01e53f52cdf48e
INADA Naoki <songofacandy@gmail.com>
2015-03-13 21:00:50 +0900

    EchoClient: Add prompt

--- EchoClient/Program.cs
+++ EchoClient/Program.cs
@@ -15,12 +15,15 @@ namespace EchoClient
       WebSocket ws = new WebSocket(args[0], null);
       ws.OnOpen = (Object sender, EventArgs e) => {
         Console.WriteLine ("Connected");
+        Console.Write("-> ");
       };
       ws.OnMessage = (Object sender, MessageEventArgs e) => {
-        Console.WriteLine (e.Data);
+        Console.WriteLine ("<- " + e.Data);
+        Console.Write("-> ");
       };
       ws.OnError = (Object sender, ErrorEventArgs e) => {
         Console.WriteLine ("ERROR: " + e.Message);
+        Console.Write("-> ");
       };
       ws.OnClose = (Object sender, CloseEventArgs e) => {
         Console.WriteLine ("Closed " + e.Code + e.Reason + e.WasClean);
